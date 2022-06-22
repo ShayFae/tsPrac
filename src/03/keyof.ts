5e6f8f56ea2550d2a871e2a9c075069241e9516e
ShayFae <ifrahnoor108@gmail.com>
2022-06-22 19:36:49 -0400

    Keyof test

--- src/03/keyof.ts
+++ src/03/keyof.ts
@@ -11,6 +11,18 @@ type ContactFields = keyof Contact;
 //It can only contain contact properties 
 const field: ContactFields = "email"; 
 
-function getValue(source, propertyName) {
+function getValue(source, propertyName: keyof Contact) {
   return source[propertyName];
 }
+
+//Using a generic function instead
+function getValue2<T>(source: T, propertyName: keyof T) {
+  return source[propertyName];
+}
+
+//Status isn't in contact so it shows an error
+const val = getValue(contact, "status")
+//No issues when using id because it is within Contact
+const val2 = getValue(contact, "id")
+
+const val3 = getValue2({min: 1, max: 100}, "max")
